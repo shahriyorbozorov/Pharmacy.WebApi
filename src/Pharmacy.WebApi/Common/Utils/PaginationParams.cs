0821c09cf781dc76890950daf5ffe39241eda672
shahriyorbozorov <shahriyorbozorov12@gmail.com>
2022-10-09 14:52:42 +0500

    paginationparams update

--- src/Pharmacy.WebApi/Common/Utils/PaginationParams.cs
+++ src/Pharmacy.WebApi/Common/Utils/PaginationParams.cs
@@ -2,19 +2,18 @@
 {
     public class PaginationParams
     {
-        private int _pageSize;
         private int _pageIndex;
-
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = value is < 0 or > 100 ? 100 : value;
-        }
+        private int _pageSize;
 
         public int PageIndex
         {
             get => _pageIndex;
             set => _pageIndex = value < 1 ? 1 : value;
+        }
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value is < 0 or > 100 ? 100 : value;
         }
     }
 }
